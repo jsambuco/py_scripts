7855a8e541d910f1133ea3f1655dc263733831ea
Joe Sambuco <sambucoj@gmail.com>
2022-09-29 16:43:44 -0400

    version 1

---
+++ lambda.py
@@ -0,0 +1,10 @@
+# AWS Lambda Examples
+# AWS SDK for Python (Boto3) to manage and invoke AWS Lambda
+# functions.
+
+import io
+import json
+import logging
+import zipfile
+from botocore.exceptions import ClientError
+
