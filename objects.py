6eb1195517bfe75f028ce0b405e26c24e7b950f1
Joe Sambuco <sambucoj@gmail.com>
2022-09-29 15:59:21 -0400

    version 2

---
+++ objects.py
@@ -0,0 +1,61 @@
+#Get the Class Name of an Instance
+class birds:
+    def name(self, name):
+        return name
+
+v = birds()
+print(v.__class__.__name__)
+
+#Example 2: Using type() and __name__ attribute
+class Vehicle:
+    def name(self, name):
+        return name
+
+v = Vehicle()
+print(type(v).__name__)
+
+#Example 1: Creating Class and Object in Python
+class Parrot:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# instantiate the Parrot class
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+# access the class attributes
+print("Blu is a {}".format(blu.__class__.species))
+print("Woo is also a {}".format(woo.__class__.species))
+
+# access the instance attributes
+print("{} is {} years old".format( blu.name, blu.age))
+print("{} is {} years old".format( woo.name, woo.age))
+
+
+# Example 2 : Creating Methods in Python
+class Parrot:
+    
+    # instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    # instance method
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+# instantiate the object
+blu = Parrot("Blu", 10)
+
+# call our instance methods
+print(blu.sing("'Happy'"))
+print(blu.dance())
