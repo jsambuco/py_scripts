f8272d5b6faf716874ba1454cc30e5f60fe5285d
Joe Sambuco <sambucoj@gmail.com>
2022-09-29 17:00:47 -0400

    version 2

--- lambda.py
+++ lambda.py
@@ -1,10 +1,30 @@
 # AWS Lambda Examples
 # AWS SDK for Python (Boto3) to manage and invoke AWS Lambda
-# functions.
+# List metrics from Cloudwatch
+# you will need boto3 installed: python3 -m pip install boto3
 
-import io
-import json
-import logging
-import zipfile
-from botocore.exceptions import ClientError
+import boto3
 
+# Create CloudWatch client
+cloudwatch = boto3.client('cloudwatch')
+
+# List metrics through the pagination interface
+paginator = cloudwatch.get_paginator('list_metrics')
+for response in paginator.paginate(Dimensions=[{'Name': 'LogGroupName'}],
+                                   MetricName='IncomingLogEvents',
+                                   Namespace='AWS/Logs'):
+    print(response['Metrics'])
+
+#Create a subscription filter¶
+import boto3
+
+# Create CloudWatchLogs client
+cloudwatch_logs = boto3.client('logs')
+
+# Create a subscription filter
+cloudwatch_logs.put_subscription_filter(
+    destinationArn='LAMBDA_FUNCTION_ARN',
+    filterName='FILTER_NAME',
+    filterPattern='ERROR',
+    logGroupName='LOG_GROUP',
+)
