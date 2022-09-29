7fbc7806aeaea2284707ead91973767044cb9847
Joe Sambuco <sambucoj@gmail.com>
2022-09-29 15:13:35 -0400

    Version 1.0

---
+++ prog_01.py
@@ -0,0 +1,6 @@
+# Check the first statement
+print("Is 2 not equal to False?", 2 != False)
+# Check the second statement
+print("Is 982*37 greater than 354*98?", 982*37 > 354*98)
+# Check the third statement
+print("Is string 'abcd' greater than 'abc'?", 'abcd' > 'abc')
